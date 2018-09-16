0b78f49e0f39bfa59d7115fc17618bb530ae2bd1
MAX <MAX@DESKTOP-3CIBT8S>
2018-09-16 11:22:02 +0300

    Added a hello comment as a test of GitHub changes button.

--- LabLesson10_09.cpp
+++ LabLesson10_09.cpp
@@ -5,7 +5,23 @@
 
 void main()
 {
-	printat(15, 15, "BBBBB!");
+	int xr;
+	int i = 1;
+
+	int k = 1;
+
+	while (true)
+	{
+		for (int i = 0; i < 1000; i++)
+		{
+			setCursorVisibility(false);
+			printat(i, i, "*");
+			pause(50);
+			printat(i, i, " ");
+		}
+	}
+	
 	waitAnyKey();
 }
 
+//hello
